c4309a6d9920d5beb221fb5595c2df07fe7ff992
Nassredean Nasseri <dean@nasseri.io>
2022-05-16 17:26:33 -0400

    add shaders

--- app.js
+++ app.js
@@ -1,37 +1,107 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import oc from 'three-orbit-controls'
+const OrbitControls = oc(THREE)
 
-export default class Sketch{
+import fragment from "./shaders/fragment.glsl";
+import vertex from "./shaders/vertex.glsl";
 
-  constructor() {
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('container').appendChild(this.renderer.domElement);
 
-    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    this.camera.position.z = 1;
+export default class Sketch {
+  constructor(options) {
     this.scene = new THREE.Scene();
 
-    this.addMesh();
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    this.camera.position.set(0, 0, 2);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.time = 0;
+
+    this.isPlaying = true;
+
+    this.addObjects();
+    this.resize();
     this.render();
+    this.setupResize();
+    // this.settings();
   }
 
-  addMesh() {
-    this.geometry = new THREE.PlaneBufferGeometry(1, 1);
-    this.material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.mesh);
+  settings() {
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    this.material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { type: "f", value: 0 },
+        resolution: { type: "v4", value: new THREE.Vector4() },
+        uvRate1: {
+          value: new THREE.Vector2(1, 1)
+        }
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.render()
+      this.isPlaying = true;
+    }
   }
 
   render() {
-    this.time++;
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.01;
-    console.log(this.time);
-
+    if (!this.isPlaying) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
     this.renderer.render(this.scene, this.camera);
-    window.requestAnimationFrame(this.render.bind(this));
   }
 }
 
-new Sketch();
+new Sketch({
+  dom: document.getElementById("container")
+});
